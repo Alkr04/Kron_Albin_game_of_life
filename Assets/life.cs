38f6f06cee2cb13743f4a6fe2f2ba00fdf45c528
Alkr04 <h23alkr@axevalla.fhsk.se>
2024-09-18 15:35:36 +0200

    added system to check stability and color changing cells

--- Assets/life.cs
+++ Assets/life.cs
@@ -6,13 +6,15 @@ using UnityEngine.SceneManagement;
 public class life : MonoBehaviour
 {
     public Vector3Int window = new Vector3Int(20, 12);
-    SpriteRenderer[,] grid = new SpriteRenderer[1000, 1000];
+    SpriteRenderer[,] grid = new SpriteRenderer[500, 500];
+    SpriteRenderer[,] sgrid = new SpriteRenderer[500, 500];
     //public GameObject[] test = new GameObject[2];
     //public GameObject t;
     public GameObject cell;
     public int cellm;
     public int frames = 1;
-    
+    int children;
+    int count = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +30,7 @@ public class life : MonoBehaviour
         {
             for (int j = 1; j < window.y - 3; j++)
             {
-                grid[i, j] = Instantiate(cell, new Vector3Int(i, j), transform.rotation).GetComponent<SpriteRenderer>();
+                grid[i, j] = Instantiate(cell, new Vector3Int(i, j), transform.rotation, gameObject.transform).GetComponent<SpriteRenderer>();
             }
         }
 
@@ -57,10 +59,10 @@ public class life : MonoBehaviour
     {
         //print(grid.GetLength(0));
         //print(test[1]);
-        
-        
-        
-        
+
+
+
+
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(0);
@@ -75,25 +77,84 @@ public class life : MonoBehaviour
                     switch (b)
                     {
                         case (0):
-                            growth(i,j);
+                            growth(i, j);
                             break;
                         case (1):
-                            death(i,j);
+                            death(i, j);
                             break;
                         case (2):
-                            decai(i,j);
+                            decai(i, j);
                             break;
                         case (3):
-                            growing(i,j);
+                            growing(i, j);
                             break;
                     }
                 }
             }
         }
-        Debug.Log(Time.deltaTime);
+        //Debug.Log(Time.deltaTime);
+        stable();
     }
 
-    
+    private void stable()
+    {
+        if (count % 16 == 0)
+        {
+            Debug.Log(count);
+            int cchildren = 0;
+            foreach (SpriteRenderer child in transform.GetComponentsInChildren<SpriteRenderer>())
+            {
+                if (child.enabled == true)
+                {
+                    cchildren++;
+                }
+            }
+            if (children == cchildren)
+            {
+                if (inspecter(grid, sgrid))
+                {
+                    Debug.Log(grid);
+                    SceneManager.LoadScene(0);
+                }
+                else
+                {
+                    sgrid = grid;
+                }
+            }
+            else
+            {
+                children = 0;
+                foreach (SpriteRenderer e in transform.GetComponentsInChildren<SpriteRenderer>())
+                {
+                    if (e.enabled == true)
+                    {
+                        children++;
+                    }
+                }
+                //children = transform.childCount;
+                sgrid = grid;
+            }
+
+            Debug.Log(cchildren);
+        }
+        count++;
+    }
+
+    bool inspecter(SpriteRenderer[,] x, SpriteRenderer[,] y)
+    {
+        for (int i = 1; i < window.x - 3; i++)
+        {
+            for (int j = 1; j < window.y - 3; j++)
+            {
+                if (x[i,j].enabled != x[i,j].enabled)
+                {
+                    return (false);
+                }
+            }
+        }
+        return (true);
+    }
+
 
     void growth(int i, int j)
     {
